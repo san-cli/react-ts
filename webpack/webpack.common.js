79c57b1a525b96c22a3c0f663e51b54eeebeaf02
Xiangzhi Chen <xiangzhi.chen@wellav.com>
2022-08-26 18:12:21 +0800

    resolve

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,6 +4,9 @@ module.exports = {
    entry: path.resolve(__dirname, "..", "./src/index.tsx"),
    resolve: {
       extensions: [".tsx", ".ts", ".js"],
+      alias: {
+         '@': path.resolve(__dirname, '..','src'),
+       },
    },
    module: {
       rules: [
